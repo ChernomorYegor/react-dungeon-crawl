491059712cd9d8f8ae72515ddb6a57a491ba3626
ChernomorYegor <chernomor.yegor@gmail.com>
2020-06-08 18:38:02 +0300

    Updates

---
+++ precache-manifest.46a7696c53b253fc35abe58b4e3d5c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a41c209bad5ea72e3020c09d90ee15",
+    "url": "/react-dungeon-crawl/index.html"
+  },
+  {
+    "revision": "4ec96c0f41312a8f589d",
+    "url": "/react-dungeon-crawl/static/css/main.c0beb38d.chunk.css"
+  },
+  {
+    "revision": "3375dd744d10880eebaf",
+    "url": "/react-dungeon-crawl/static/js/2.834754ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-dungeon-crawl/static/js/2.834754ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec96c0f41312a8f589d",
+    "url": "/react-dungeon-crawl/static/js/main.7c0ea5f1.chunk.js"
+  },
+  {
+    "revision": "d33d43ed28b31483bb64",
+    "url": "/react-dungeon-crawl/static/js/runtime-main.eb623704.js"
+  }
+]);
